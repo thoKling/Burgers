8493a6d3a21413a264431d9d39f1fa7f1f77f654
Thomas KLING <klingthomas67@gmail.com>
2020-03-12 08:46:19 +0100

    Ajout de bootstrap et methode pour recuperer les nutriments

--- Gemfile
+++ Gemfile
@@ -28,6 +28,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# Bootstrap
+gem 'bootstrap', '~> 4.0.0'
+# JQuery
+gem 'jquery-rails'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,2 +1,39 @@
 class Burger < ApplicationRecord
+    def get_nutriments
+        # Créé un nouveau tableau
+        res = Array.new
+        # si le code du burger n'est pas nul
+        if(!self.code.nil?)
+            # on recherche l'objet correspondant au code
+            product = Openfoodfacts::Product.get(self.code, locale: 'fr')
+            # si le produit correspondant existe
+            if(product.present?)
+                # alors on recherche ses nutriments
+                nutri = product.nutriments.to_hash
+                # S'il en a
+                if nutri.count != 0
+                    # pour chacun des nutriments
+                    for ele in nutri
+                        # si il n'est pas une donnee d'unite
+                        if !ele[0].to_s.include? "unit"
+                            nutri = Array.new
+                            nutri.push(ele[0].to_s.gsub('_', ' ').gsub('-', ' '))
+                            nutri.push(ele[1].to_s.gsub('_', ' ').gsub('-', ' '))
+                            # on l'ajoute a notre tableau
+                            res.push(nutri)
+                         end
+                    end
+                end
+            end
+        end
+
+        if(res.count == 0)
+            nutri = Array.new
+            nutri.push("No element")
+            nutri.push("ZERO NULL NADA QUE TCHI GUCCI")
+            # on l'ajoute a notre tableau
+            res.push(nutri)
+        end
+        return res
+    end
 end
